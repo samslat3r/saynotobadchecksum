9870492bd67c0c9cbfa5dee4937db152cfe22d32
Sam Slater <sslater@posteo.net>
2025-11-06 23:15:43 -0800

    Feat (infra): add CORS config to uploads bucket

--- modules/s3_uploads/main.tf
+++ modules/s3_uploads/main.tf
@@ -1,37 +1,49 @@
 resource "aws_s3_bucket" "main" {
-    bucket = var.bucket_name
-    force_destroy = false
-    tags = var.tags
+  bucket        = var.bucket_name
+  force_destroy = false
+  tags          = var.tags
 }
 
 resource "aws_s3_bucket_public_access_block" "main" {
-    bucket = aws_s3_bucket.main.id
-    block_public_acls = true
-    block_public_policy = true
-    ignore_public_acls = true
-    restrict_public_buckets = true
+  bucket                  = aws_s3_bucket.main.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_versioning" "main" {
-    bucket = aws_s3_bucket.main.id
-    versioning_configuration { status = "Enabled" }
+  bucket = aws_s3_bucket.main.id
+  versioning_configuration { status = "Enabled" }
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "main" {
-    bucket = aws_s3_bucket.main.id
+  bucket = aws_s3_bucket.main.id
 
-    rule {
-        apply_server_side_encryption_by_default {
-            sse_algorithm = "AES256"
-        }
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
+  }
 }
 
 resource "aws_s3_bucket_lifecycle_configuration" "main" {
-    bucket = aws_s3_bucket.main.id
-    rule {
-        id = "expire-noncurrent"
-        status = "Enabled"
-        noncurrent_version_expiration { noncurrent_days = 30 }
-    }
+  bucket = aws_s3_bucket.main.id
+  rule {
+    id     = "expire-noncurrent"
+    status = "Enabled"
+    noncurrent_version_expiration { noncurrent_days = 30 }
+  }
+}
+
+resource "aws_s3_bucket_cors_configuration" "main" {
+  bucket = aws_s3_bucket.main.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "PUT", "POST"]
+    allowed_origins = ["*"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3600
+  }
 }
