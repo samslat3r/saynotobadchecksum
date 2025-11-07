9463086637902fca06e575f30a7f11520fb32552
Sam Slater <sslater@posteo.net>
2025-11-06 23:04:44 -0800

    fix terraform: correct lambda_artifacts default paths

--- infra/environments/prod/variables.tf
+++ infra/environments/prod/variables.tf
@@ -1,53 +1,53 @@
 variable "project_name" {
-    type = string 
+  type = string
 }
 
 variable "env" {
-    type = string 
+  type = string
 }
 
 variable "aws_region" {
-    type = string 
+  type = string
 }
 
 variable "bucket_name" {
-    type = string
-    default = ""
+  type    = string
+  default = ""
 
 }
 
-variable "ddb_table_name" { 
-    type = string
-    default = ""
+variable "ddb_table_name" {
+  type    = string
+  default = ""
 }
 
 variable "lambda_artifacts" {
-    description = "Paths to built lambda zips"
-    type = object({
-      presign = string
-      list_files = string
-      process_upload = string
-    })
-    default = {
-        presign = "../../../lambdas"
-        list_files = "../../../lambdas"
-        process_upload = "../../../lambdas"
-    }
+  description = "Paths to built lambda zips"
+  type = object({
+    presign        = string
+    list_files     = string
+    process_upload = string
+  })
+  default = {
+    presign        = "../../../lambdas/presign/dist-presign.zip"
+    list_files     = "../../../lambdas/list_files/dist-list_files.zip"
+    process_upload = "../../../lambdas/process_upload/dist-process_upload.zip"
+  }
 }
 
 # OIDC Metadata (not secrets)
 
-variable github_owner {
-    type = string
-    default = "samslat3r"  
+variable "github_owner" {
+  type    = string
+  default = "samslat3r"
 }
 
-variable github_repo {
-    type = string
-    default = "saynotobadchecksum"
+variable "github_repo" {
+  type    = string
+  default = "saynotobadchecksum"
 }
 
-variable github_branch {
-    type = string
-    default = "master"
+variable "github_branch" {
+  type    = string
+  default = "master"
 }
