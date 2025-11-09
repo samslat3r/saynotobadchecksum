6cc9da48bfa2be328cfc6799a12b6aca2dc574ba
Sam Slater <sslater@posteo.net>
2025-11-09 11:16:49 -0800

    comment out import blocks to prevent calidation errors and import attempts on every terraform plan/apply

--- infra/environments/staging/main.tf
+++ infra/environments/staging/main.tf
@@ -32,45 +32,48 @@ locals {
 # These will automatically import resources if they exist outside of state
 # Can be removed after initial import is complete
 
-import {
-  to = module.s3.aws_s3_bucket.main
-  id = "sam-secure-staging-uploads"
-}
+# COMMENTED OUT: Enable these after staging infrastructure is initially deployed
+# Then these blocks will handle importing any resources created manually
 
-import {
-  to = module.ddb.aws_dynamodb_table.main
-  id = "sam-secure-staging-uploads"
-}
+# import {
+#   to = module.s3.aws_s3_bucket.main
+#   id = "sam-secure-staging-uploads"
+# }
 
-import {
-  to = module.iam.aws_iam_role.lambda
-  id = "sam-secure-staging-lambda-role"
-}
+# import {
+#   to = module.ddb.aws_dynamodb_table.main
+#   id = "sam-secure-staging-uploads"
+# }
 
-import {
-  to = module.secrets.aws_secretsmanager_secret.presign
-  id = "sam-secure-staging-presign"
-}
+# import {
+#   to = module.iam.aws_iam_role.lambda
+#   id = "sam-secure-staging-lambda-role"
+# }
 
-import {
-  to = module.secrets.aws_secretsmanager_secret.vt
-  id = "sam-secure-staging-vt-api-key"
-}
+# import {
+#   to = module.secrets.aws_secretsmanager_secret.presign
+#   id = "sam-secure-staging-presign"
+# }
 
-import {
-  to = module.web_s3.aws_s3_bucket.web
-  id = "saynotobadchecksum-web-staging-${data.aws_caller_identity.current.account_id}"
-}
+# import {
+#   to = module.secrets.aws_secretsmanager_secret.vt
+#   id = "sam-secure-staging-vt-api-key"
+# }
 
-import {
-  to = module.gha_oidc.aws_iam_openid_connect_provider.github
-  id = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"
-}
+# import {
+#   to = module.web_s3.aws_s3_bucket.web
+#   id = "saynotobadchecksum-web-staging-${data.aws_caller_identity.current.account_id}"
+# }
 
-import {
-  to = module.gha_oidc.aws_iam_role.gha
-  id = "sam-secure-staging-gha-oidc"
-}
+# import {
+#   to = module.gha_oidc.aws_iam_openid_connect_provider.github
+#   id = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"
+# }
+
+# import {
+#   to = module.gha_oidc.aws_iam_role.gha
+#   id = "sam-secure-staging-gha-oidc"
+# }
 
 # Note: Lambda functions will be created fresh if they don't exist
 # Uncomment these if Lambda functions exist and need to be imported:
